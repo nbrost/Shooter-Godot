85b4065cd14d4cecee60774dd4ca9fc9cd8a073d
nolan <nbrost@ualberta.ca>
2024-07-15 23:06:20 -0600

    simple ui

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -1,11 +1,9 @@
 extends LevelParent
 
+@export var outside_level_scene: PackedScene 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_area_2d_body_entered(_body):
+	var tween = create_tween()
+	tween.tween_property($Player, "speed", 0, 0.5)
+	# get_tree().change_scene_to_file("res://scenes/levels/outside.tscn")
+	get_tree().change_scene_to_packed(outside_level_scene)
